281d7f1390e2e2300a2280e13dce51af7ac2411b
bradyshehu <shehu.brady@gmail.com>
2025-07-01 16:22:55 +0200

    add logic and routing of the two index for bestsellers and recents

--- routers/parfumesRouter.js
+++ routers/parfumesRouter.js
@@ -3,7 +3,12 @@ const parfumesRouter = express.Router();
 const parfumesController = require("../controllers/parfumesController.js");
 
 // # Rotte già gestite
+// INDEX
 parfumesRouter.get("/", parfumesController.index);
+parfumesRouter.get("/bestsellers", parfumesController.indexBestSellers);
+parfumesRouter.get("/recents", parfumesController.indexRecents);
+
+// SHOW
 parfumesRouter.get("/:id", parfumesController.show);
 
 module.exports = parfumesRouter;
